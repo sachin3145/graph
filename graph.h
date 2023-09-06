3e374f8715c82d2d728a1e313d35f9b3e52b729a
unknown <sachin3145@outlook.com>
2023-09-06 18:44:25 +0530

    INITIATED HEADER

--- graph.h
+++ graph.h
@@ -1,1 +1,19 @@
 #pragma once
+#include <vector>
+
+class graph
+{
+private:
+  std::vector<std::vector<int>> am;
+
+public:
+  graph(/* args */);
+  ~graph();
+};
+
+graph::graph(/* args */){
+}
+
+graph::~graph()
+{
+}
